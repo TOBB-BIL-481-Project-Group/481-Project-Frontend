2c518256e47d287e19ca1a649ec8023c738bc0f3
AKaan37 <akaan372742@gmail.com>
2024-04-08 02:47:44 +0300

    Signup Bug Fixed

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'; // Import React and useState
+import { useState } from "react"; // Import React and useState
 import { PATHS } from "../consts/paths";
-import { Link} from 'react-router-dom'; // Assuming you're using React Router
-import { apisignup } from '../react-query/queries'; // Importing the apisignup function
-import { FormEvent } from 'react'; // Import FormEvent type from react
+import { Link } from "react-router-dom"; // Assuming you're using React Router
+import { apisignup } from "../react-query/queries"; // Importing the apisignup function
+import { FormEvent } from "react"; // Import FormEvent type from react
 import { useNavigate } from "react-router-dom";
 
 const StartPage = () => {
   // State variables to store form data
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   // Function to handle form submission
@@ -17,53 +17,99 @@ const StartPage = () => {
     event.preventDefault(); // Prevent default form submission behavior
 
     // Collect form data
-    if(name === '' || email === '' || password === ''){
+    if (name === "" || email === "" || password === "") {
       alert("Please fill all the blanks!");
       return;
     }
     try {
-      await apisignup(name,email,password,"/");
-      navigate(PATHS.loading);  // Log successful response
+      await apisignup(name, email, password, "/");
+      navigate(PATHS.loading); // Log successful response
     } catch (error) {
-      navigate(PATHS.error); // Log any errors
+      if (error === "AxiosError: Request failed with status code 500")
+        alert("email's account not found");
+      else alert("your password is not correct");
     }
   };
 
   return (
-    <div className="home" style={{
-      backgroundColor: "#3d424d",
-      fontFamily: "Arial",
-      display: "flex",
-      textAlign: "center",
-      justifyContent: "center",
-      alignItems: "center",
-      height: "100vh",
-      flexDirection: "column"
-    }}>
-      <div className="welcome-header" style={{
+    <div
+      className="home"
+      style={{
+        backgroundColor: "#3d424d",
+        fontFamily: "Arial",
+        display: "flex",
         textAlign: "center",
-        marginBottom: "30px",
-        fontSize: "24px",
-        color: "white",
-      }}>
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <div
+        className="welcome-header"
+        style={{
+          textAlign: "center",
+          marginBottom: "30px",
+          fontSize: "24px",
+          color: "white",
+        }}
+      >
         <h1>Welcome to the Test-Generator Site</h1>
       </div>
-      <form onSubmit={handleSubmit} method='post' className="form-container">
-        <label htmlFor="name" className="form-label">Name:</label><br />
-        <input type="text" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} className="custom-input" /><br /><br />
-        
-        <label htmlFor="email" className="form-label">Email:</label><br />
-        <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} className="custom-input"/><br /><br />
-        
-        <label htmlFor="password" className="form-label">Password:</label><br />
-        <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} className="custom-input" /><br /><br />
-        
+      <form onSubmit={handleSubmit} method="post" className="form-container">
+        <label htmlFor="name" className="form-label">
+          Name:
+        </label>
+        <br />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          className="custom-input"
+        />
+        <br />
+        <br />
+
+        <label htmlFor="email" className="form-label">
+          Email:
+        </label>
+        <br />
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="custom-input"
+        />
+        <br />
+        <br />
+
+        <label htmlFor="password" className="form-label">
+          Password:
+        </label>
+        <br />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="custom-input"
+        />
+        <br />
+        <br />
+
         <input type="submit" value="Log In" className="submit-button" />
         <br />
         <Link to={PATHS.signup} className="signup-link">
-          <button className="signup-button">If you do not have an account, please sign up</button>
+          <button className="signup-button">
+            If you do not have an account, please sign up
+          </button>
         </Link>
-    </form>
+      </form>
     </div>
   );
 };
