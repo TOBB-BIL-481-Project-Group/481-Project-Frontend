77f2ae539ed0164565ae984670d1dcb5063522f3
kbekleyen <kaanmbekleyen@gmail.com>
2024-04-08 06:35:26 +0300

    About us redesigned, home page style issues fixed
    Home page image carousel had a bug that changed its height. The height is now constant.

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -3,8 +3,15 @@ import { PATHS } from "../consts/paths";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavItem } from 'react-bootstrap';
+
+
 const AboutUs = () =>{
-  return(<div>
+  return(<div style={{ 
+    backgroundImage: `url(https://wallpapercave.com/wp/wp8063327.jpg)`, // Adjust the path to your image
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    minHeight: "100vh",
+}}>
     <Navbar bg="dark" data-bs-theme="dark" className='justify-content-center '>
       <Navbar.Brand>
         About Us
@@ -22,23 +29,37 @@ const AboutUs = () =>{
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    <h1>About Us</h1>
-    <h3>Developers</h3>
-    <ul>
-      <li>
-        Ahmet Kaan Avcı
-      </li>
-      <li>
-        Esat Okcu
-      </li>
-      <li>
-        Kerem Kazandır
-      </li>
-      <li>
-        Kaan Mert Bekleyen
-      </li>
-    </ul>
-    <h4>Special Thanks to Faruk Avcı</h4>
+    <div className='text-center'>
+    <br></br>
+            <h1>About Us</h1>
+            <p>Welcome to our Testcase Generator! We are a team of passionate developers dedicated to creating efficient tools for software testing and development.</p>
+            
+            <br></br>
+            <h2>Meet Our Team</h2>
+            <br></br>
+            <div>
+                <h4>Ahmet Kaan Avcı</h4>
+                <p>Backend Developer</p>
+            </div>
+            
+            <div>
+                <h4>Kerem Kazandır</h4>
+                <p>Backend Developer</p>
+            </div>
+
+            <div>
+                <h4>Esat Okcu</h4>
+                <p>Frontend Developer</p>
+            </div>
+
+            <div>
+                <h4>Kaan Mert Bekleyen</h4>
+                <p>Frontend Developer</p>
+            </div>
+            <br></br>
+            <h4>Special Thanks to Faruk Avcı</h4>
+        </div>
+    
   </div>)
 };
 export default AboutUs;
